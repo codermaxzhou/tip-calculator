8e56446a964c5b17fa77c825dcce22dd5190adf0
Max Zhou <6313700+yihaozhou@users.noreply.github.com>
2019-10-11 00:46:13 -0400

    create tip calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tip-calculator';
+  taxRate = 0;
+  tipRates: number[] = [10, 12, 15, 18, 20];
+  selectedTipRate: number = this.tipRates[0]; // default rate is 10
+  items: Item[] = [
+    { salePrice: 0, finalPrice: 0, tip: 0, tax: 0 }
+    ];
+  total: Item = { salePrice: 0, finalPrice: 0, tip: 0, tax: 0 };
+
+  calculate(item: Item) {
+    item.tip = item.salePrice * this.selectedTipRate / 100;
+    item.tax = item.salePrice * this.taxRate / 100;
+    item.finalPrice = item.salePrice + item.tax + item.tip;
+  }
+
+  calculateAll() {
+    this.total = { salePrice: 0, finalPrice: 0, tip: 0, tax: 0 };
+    for (const item of this.items) {
+      this.calculate(item);
+      this.total.salePrice += item.salePrice;
+    }
+    this.calculate(this.total);
+  }
+
+  addItem() {
+    this.items.push({ salePrice: 0, finalPrice: 0, tip: 0, tax: 0 });
+  }
+
+  removeItem(index) {
+    if (index > -1) {
+      this.items.splice(index, 1);
+    }
+    this.calculateAll();
+  }
+}
+
+interface Item {
+  salePrice: number;
+  finalPrice: number;
+  tip: number;
+  tax: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatRadioModule, MatSliderModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,13 @@ import { MatCardModule } from '@angular/material';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatSliderModule,
+    FormsModule,
+    MatRadioModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
